b109be180fcd5addee73fa1a9880124ef34ee4ef
Ahmad <ahmad.i@develeap.com>
2019-12-16 14:56:36 +0200

    update domain

--- scripts/run_nginx.sh
+++ scripts/run_nginx.sh
@@ -2,7 +2,7 @@
 
 echo "################################## Run nginx"
 export DOLLAR='$'
-export DOMAIN='localhost'
+export DOMAIN='onlinedeveleap.tk'
 envsubst < ./src/configs/nginx/nginx.conf.template > /etc/nginx/nginx.conf # /etc/nginx/conf.d/default.conf
 nginx -g "daemon off;"
 
